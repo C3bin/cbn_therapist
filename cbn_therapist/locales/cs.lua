f8b02fb2a2317da6dec551bbb9850a7c763c05d6
C3bin <77668443+C3bin@users.noreply.github.com>
2023-05-28 13:29:46 +0200

    Fixed illenium appearance not working.

---
+++ cbn_therapist/locales/cs.lua
@@ -0,0 +1,14 @@
+Locales['cs'] = {
+    -- ox_lib Context Menu
+    ['startMeet'] = 'Zaplatit za schůzku.',
+    ['requestMeet'] = 'Dojednat si schůzku s Terapeutem.',
+    -- notifikace
+    ['no_money'] = 'Nemáš peníze.',
+    ['feel_better'] = 'Navštívil jsi Terapeuta, už se cítíš lépe.',
+    ['feel_better_stress'] = 'Navštívil jsi Terapeuta, tvůj stress byl snížen.',
+    -- ox_target / qtarget
+    ['deal_meeting'] = 'Promluvit si s Terapeutem',
+    -- ox_lib Progress Bar
+    ['dealing_meeting'] = 'Dohaduješ se...',
+    ['talking'] = 'Rozpráváš se...'
+}
