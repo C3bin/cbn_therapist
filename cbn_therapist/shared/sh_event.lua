03239786acef56b6bf70c189037cef91cce37faf
C3bin <77668443+C3bin@users.noreply.github.com>
2023-05-31 14:47:31 +0200

    Delete cbn_therapist directory

--- cbn_therapist/shared/sh_event.lua
+++
@@ -1,4 +0,0 @@
-AddEventHandler("cbn_therapist:relieveStress", function(source)
-    TriggerServerEvent('hud:server:RelieveStress', Config.StressAmount) -- Here you can add your own custom event/export
-end)
-
